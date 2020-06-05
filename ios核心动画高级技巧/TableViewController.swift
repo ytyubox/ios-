8f01805bd3a8f09c2d8bef1e1099055b3ad655e5
Tsungyu Yu <ytyubox@gmail.com>
2020-06-05 17:50:11 +0800

    apply navigation

---
+++ ios核心动画高级技巧/TableViewController.swift
@@ -0,0 +1,28 @@
+//
+//  TableViewController.swift
+//  ios核心动画高级技巧
+//
+//  Created by 游宗諭 on 2020/6/5.
+//  Copyright © 2020 游宗諭. All rights reserved.
+//
+
+import UIKit
+class TableViewController:UITableViewController {
+    
+    let chapters:[UIViewController]  = [
+        ViewController_ch1()
+    ]
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        chapters.count
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "\(type(of: chapters[indexPath.row]))"
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        navigationController?.pushViewController(chapters[indexPath.row], animated: true)
+    }
+}
